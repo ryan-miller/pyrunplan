f5fc6c12950acb4dffe4c63b6cdfac97981c078e
Ryan Miller <ryan.l.miller@icloud.com>
2022-05-12 20:20:28 -0700

    adding startMinutes option, which includes weekly total Minutes

--- pyrunplan.py
+++ pyrunplan.py
@@ -1,8 +1,9 @@
 class PyRunPlan:
 
     def __init__(self):
-        self._planLength = 0
+        self._planLength = 4
         self._plan = list()
+        self._startMinutes = 0
 
     @property
     def plan(self):
@@ -15,10 +16,23 @@ class PyRunPlan:
     def planLength(self, value):
         self._planLength = value
 
+    @property
+    def startMinutes(self):
+        return self._startMinutes
+    @startMinutes.setter
+    def startMinutes(self, value):
+        self._startMinutes = value
+
     def calculate(self):
 
         for x in range(self.planLength):
             print(x)
-            self.plan.append(list())
+            temp = dict()
+            temp["totalMinutes"] = self.startMinutes
+            self.plan.append(temp)
 
         return self.plan
+
+    def week(self, value):
+
+        return self.plan[value]
